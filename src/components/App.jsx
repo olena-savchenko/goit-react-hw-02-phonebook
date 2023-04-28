10f9ea9f2e9d7bf3a2af0dd83cc18f791bcbe2e3
Olena Savchenko <elena.volobuieva@gmail.com>
2023-04-28 23:48:09 +0300

    add ContactForm components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Test
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  addContact = contact => {
+    // console.log('контакт в APP: ', contact);
+    // console.log(this.state);
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+  };
+
+  render() {
+    // console.log(this.state);
+    // console.log('Massiv this.state.contacts: ', this.state.contacts);
+    // const { items } = this.state.contacts;
+
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(contact => {
+            return (
+              <li key={contact.name}>
+                {contact.name}: {contact.number}
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = e => {
+    console.log(e.currentTarget.value);
+
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state);
+    this.props.addContact(this.state);
+    this.reset();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <label htmlFor="">
+          Name
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label htmlFor="">
+          Number
+          <input
+            type="tel"
+            name="number"
+            value={this.state.number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <button tpe="submit">Add contact </button>
+      </form>
+    );
+  }
+}
